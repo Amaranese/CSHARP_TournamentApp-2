88af3146e63105332a3ff337e89db7ee867c7f51
Amaranese <alejandromaranes@gmail.com>
2022-04-27 10:34:44 +0200

    Create CreateTeamForm.cs

---
+++ TournamentTracker/TrackerUI/CreateTeamForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrackerUI
+{
+    public partial class CreateTeamForm : Form
+    {
+        public CreateTeamForm()
+        {
+            InitializeComponent();
+        }
+
+        private void d_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateTeamForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emailLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cellPhoneLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
