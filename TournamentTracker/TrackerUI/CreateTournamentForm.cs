98450698efb8e9b21362dc584dcbe4898d71fef5
Amaranese <alejandromaranes@gmail.com>
2022-04-27 10:34:16 +0200

    Create CreateTournamentForm.cs

---
+++ TournamentTracker/TrackerUI/CreateTournamentForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrackerUI
+{
+    public partial class CreateTournamentForm : Form
+    {
+        public CreateTournamentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void teamOneScoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teamOneScoreText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateTournamentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void entryFeeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void entryFeeValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
